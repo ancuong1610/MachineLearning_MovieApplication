40c49cb438bbf82bbccbae16e4051a33dfd1b9da
Vanessa Demudia <demudiavanessa@gmail.com>
2024-01-27 23:30:32 +0100

    app

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+from string import Template
+from SPARQLWrapper import SPARQLWrapper, JSON
+sparql = SPARQLWrapper("http://localhost:3030/TMDB/sparql")
+sparql.setReturnFormat(JSON)
+actor_query = """
+PREFIX : <https://www.themoviedb.org/kaggle-export/>
+SELECT ?actor
+WHERE{
+?m a :Movie ;
+:title "$title" ;
+:cast/:name ?actor .
+}
+"""
+def query(title):
+    query_string = Template(actor_query).substitute(title=title)
+    sparql.setQuery(query_string)
+    results_dict = sparql.query().convert()
+    results = [row['actor']['value'] for row in results_dict['results']['bindings']]
+    return results
+
+app = Flask(__name__)
+@app.route("/", methods=('GET', 'POST'))
+def index():
+    if request.method == 'POST':
+        title = request.form['title']
+        return render_template('index.html', actors=query(title))
+    else:
+        return render_template('index.html', actors=[])
