f9ea3140af0431b2cb0465d691d587a115218420
antenne <anton.vikulov@stud.h-da.de>
2024-02-03 12:01:05 +0100

    feat: add NLP functionality to get movie description based on a given title

--- handler/NLPHandler.py
+++ handler/NLPHandler.py
@@ -1,8 +1,25 @@
-# Placeholder for NLP functionality
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neighbors import NearestNeighbors
+
+
 class NLPHandler:
-    @staticmethod
-    def get_movie_description(title):
-        # Implement NLP logic here to get the description based on the title
-        # This could involve using pre-trained models or external APIs
-        # Return a placeholder description for now
-        return f"Description for {title} using NLP"
+    def __init__(self):
+        self.df = pd.read_csv('../templates/tmdb_5000_movies.csv')
+
+        # Not available values to empty String
+        self.df['overview'] = self.df['overview'].fillna('')
+        self.vectorizer = TfidfVectorizer()
+
+        # overview column = description
+        self.tfidf_matrix = self.vectorizer.fit_transform(self.df['overview'])
+
+        self.model = NearestNeighbors(metric='cosine')
+        self.model.fit(self.tfidf_matrix)
+
+    def get_movie_description(self, title):
+        title_vector = self.vectorizer.transform([title])
+        distances, indices = self.model.kneighbors(title_vector)
+        # index of nearest movie
+        index = indices[0][0]
+        return self.df.loc[index, 'overview']
